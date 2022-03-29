20b370d8a5f60c3042f4523bf4ad5eab6abbb512
Brian Yee <brian.yee@sil.org>
2022-03-29 10:08:59 +0100

    translate to french

--- check_log_file.py
+++ check_log_file.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
 
-# Developed by Brian Yee and Nate Marti.
-
-# Import needed commands from modules.
+# Importer des modules dont nous avons besoin
 import sys
 import time
 from pathlib import Path
 from datetime import datetime,timezone,timedelta
 
-# Set global variable.
-DAY_IN_SECONDS = 24 * 60 * 60
-OFFSET = 4 * DAY_IN_SECONDS
+# définition variable globale
+JOUR_EN_SECONDS = 24 * 60 * 60
+OFFSET = 4 * JOUR_EN_SECONDS
 
-# Creates a date OFFSET before "now"
-example_date = datetime.fromtimestamp(time.time() - OFFSET, timezone(timedelta(hours=1)))
+# Créer une date OFFSET avant ce moment-ci
+exemple_date = datetime.fromtimestamp(time.time() - OFFSET, timezone(timedelta(hours=1)))
 
-found = 0
-# Path will adapt the path to be platform specific.
-# This is a sample file that looks like an apache log file.
-path_to_file = Path("./datafiles/log_file")
+trouve = 0
+# Path va aller selon la plateforme utilisée
+# Ce fichier est un journal qui est comme un journal Apache
+chemin_de_fichier = Path("./datafiles/log_file")
 
-if path_to_file.exists():
-    with path_to_file.open("r", encoding="UTF-8") as log_file:
-        lines = log_file.readlines()
-        for line in lines:
-            raw_line = line.strip()
+if chemin_de_fichier.exists():
+    with chemin_de_fichier.open("r", encoding="UTF-8") as log_file:
+        lignes = log_file.readlines()
+        for ligne in lignes:
+            ligne_crue = ligne.strip()
 
             # Separate out date from rest of line
-            substring_start = raw_line.find('[')
-            substring_end = raw_line.find(']')
-            date_part_of_line = raw_line[(substring_start+1):(substring_end)]
+            substring_debut = ligne_crue.find('[')
+            substring_fin = ligne_crue.find(']')
+            date_de_ligne = ligne_crue[(substring_debut+1):(substring_fin)]
 
             # Turn string into datetime object
-            log_date = datetime.strptime(date_part_of_line, "%d/%b/%Y:%H:%M:%S %z")
+            journal_date = datetime.strptime(date_de_ligne, "%d/%b/%Y:%H:%M:%S %z")
 
-            if log_date > example_date:
-                found = 1
+            if journal_date > exemple_date:
+                trouve = 1
 else:
-    print("Error: opening file" + str(path_to_file), file=sys.stderr)
+    print("Erreur: opening file" + str(chemin_de_fichier), file=sys.stderr)
     exit(1)
 
 
-if found:
-    print(f"Log file entry found since {example_date}")
+if trouve:
+    print(f"Il y a du contentu du journal trouvé depuis {exemple_date}")
 else:
-    print(f"No recent log file entry found since {example_date}")
+    print(f"Aucun contenu du journal trouvé depuis {exemple_date}")
